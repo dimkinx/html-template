e51fec97c45c5a01eedeee242e390397cd22a7ca
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2023-04-12 15:20:41 +0300

    Apply stylelint config clean-order

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+import { iosVhFix } from './utils/ios-vh-fix';
+import { initModals } from './modules/modals/init-modals';
+
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Utils
+  // ---------------------------------
+
+  iosVhFix();
+
+  // Modules
+  // ---------------------------------
+
+  window.addEventListener('load', () => {
+    initModals();
+  });
+});
+
+// .is-active || .is-open || .is-invalid
+// .select.select--opened ❌ ---> [data-select].is-open ✅
+
+// const breakpoint = window.matchMedia(`(min-width:1024px)`);
+// const breakpointChecker = () => {
+//   if (breakpoint.matches) {
+//   } else {
+//   }
+// };
+// breakpoint.addListener(breakpointChecker);
+// breakpointChecker();
+
+// .closest(el)
