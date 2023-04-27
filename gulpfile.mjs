ff9ef19b3e8aa18fb3e20a5825cd2435606ad012
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2023-04-27 22:37:55 +0300

    Refactor gulp tasks

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,28 +1,26 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import gulp from 'gulp';
 import config from './gulp/config.mjs';
-import clean from './gulp/tasks/clean.mjs';
-import compileHtml from './gulp/tasks/compile-html.mjs';
-import {compileStyles} from './gulp/tasks/compile-styles.mjs';
+import clearBuild from './gulp/tasks/clear-build.mjs';
 import {initServer, streamServer, reloadServer} from './gulp/tasks/server.mjs';
+import compileHtml from './gulp/tasks/compile-html.mjs';
+import compileStyles from './gulp/tasks/compile-styles.mjs';
+import compileJs from './gulp/tasks/compile-js.mjs';
+
+config.setEnv();
 
 const streamStyles = () => streamServer(compileStyles);
 
-const watch = () => {
+const watchFiles = () => {
   gulp.watch(config.path.watch.html, gulp.series(compileHtml, reloadServer));
   gulp.watch(config.path.watch.scss, streamStyles);
+  gulp.watch(config.path.watch.js, gulp.series(compileJs, reloadServer));
 };
 
-const dev = gulp.series(
-  clean,
-  gulp.parallel(
-    compileHtml,
-    compileStyles,
-  ),
-  gulp.parallel(
-    initServer,
-    watch,
-  ),
-);
+const syncServer = gulp.parallel(initServer, watchFiles);
+const tasks = gulp.parallel(compileHtml, compileStyles, compileJs);
 
-gulp.task('default', dev);
+const start = gulp.series(clearBuild, tasks, syncServer);
+const build = gulp.series(clearBuild, tasks);
+
+export {start, build};
