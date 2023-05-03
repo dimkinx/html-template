024752c025d7b463396ccdc6e968e7d8666739ee
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2023-05-03 20:11:17 +0300

    Refactor gulp task 'copy assets'

--- gulp/tasks/copy-assets.mjs
+++ gulp/tasks/copy-assets.mjs
@@ -1,16 +1,29 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 import gulp from 'gulp';
 import config from '../config.mjs';
 
 const copyAssets = () => gulp
   .src([
-    config.path.source.woff,
-    config.path.source.img,
-    config.path.source.svg,
     config.path.source.favicon,
+    config.path.source.woff,
+    config.path.source.jpg,
+    config.path.source.png,
+    config.path.source.svg,
   ], {
     base: 'source',
   })
-  .pipe(gulp.dest('build'));
+  .pipe(gulp.dest(config.path.buildFolder));
 
-export default copyAssets;
+const copyJpg = () => gulp
+  .src(config.path.source.jpg, {base: 'source'})
+  .pipe(gulp.dest(config.path.buildFolder));
+
+const copyPng = () => gulp
+  .src(config.path.source.png, {base: 'source'})
+  .pipe(gulp.dest(config.path.buildFolder));
+
+const copySvg = () => gulp
+  .src(config.path.source.svg, {base: 'source'})
+  .pipe(gulp.dest(config.path.buildFolder));
+
+export {copyAssets, copyJpg, copyPng, copySvg};
