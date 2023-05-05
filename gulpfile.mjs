1e3114012ae50f237798f41b5aaa6052010f0e6b
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2023-05-05 20:14:36 +0300

    Refactor gulp file

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -3,29 +3,46 @@ import gulp from 'gulp';
 import config from './gulp/config.mjs';
 import clearBuild from './gulp/tasks/clear-build.mjs';
 import {initServer, streamServer, reloadServer} from './gulp/tasks/server.mjs';
-import copyAssets from './gulp/tasks/copy-assets.mjs';
+import {copyAssets, copyJpg, copyPng, copySvg} from './gulp/tasks/copy-assets.mjs';
 import createSprite from './gulp/tasks/create-sprite.mjs';
 import compileHtml from './gulp/tasks/compile-html.mjs';
 import compileStyles from './gulp/tasks/compile-styles.mjs';
 import compileJs from './gulp/tasks/compile-js.mjs';
-import {convertTtfToWoff, convertTtfToWoff2} from './gulp/tasks/convert-fonts.mjs';
+import convertFonts from './gulp/tasks/convert-fonts.mjs';
+import createWebp from './gulp/tasks/create-webp.mjs';
+import {optimizeJpg, optimizePng, optimizeSvg} from './gulp/tasks/optimize-images.mjs';
 
 config.setEnv();
 
 const streamStyles = () => streamServer(compileStyles);
+
+const optimizeImages = async () => gulp.parallel(optimizeSvg, optimizeJpg, optimizePng);
+
+const build = gulp.series(
+  clearBuild,
+  createWebp,
+  copyAssets,
+  optimizeImages,
+  createSprite,
+  gulp.parallel(
+    compileHtml,
+    compileStyles,
+    compileJs,
+  ),
+);
 
 const watchFiles = () => {
   gulp.watch(config.path.watch.html, gulp.series(compileHtml, reloadServer));
   gulp.watch(config.path.watch.scss, streamStyles);
   gulp.watch(config.path.watch.js, gulp.series(compileJs, reloadServer));
   gulp.watch(config.path.watch.icons, gulp.series(createSprite, compileHtml, reloadServer));
+  gulp.watch(config.path.watch.jpg, gulp.series(createWebp, copyJpg, optimizeJpg, compileHtml, reloadServer));
+  gulp.watch(config.path.watch.png, gulp.series(createWebp, copyPng, optimizePng, compileHtml, reloadServer));
+  gulp.watch(config.path.watch.svg, gulp.series(copySvg, optimizeSvg, compileHtml, reloadServer));
 };
-
-const convertFonts = gulp.series(convertTtfToWoff, convertTtfToWoff2);
 
 const syncServer = gulp.parallel(initServer, watchFiles);
 
-const build = gulp.series(clearBuild, copyAssets, createSprite, gulp.parallel(compileHtml, compileStyles, compileJs));
 const start = gulp.series(build, syncServer);
 
 export {convertFonts, build, start};
